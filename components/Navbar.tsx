16943484a27f431e1504547eefc84420580faee5
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-10-02 13:27:10 +0200

    add Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Logo from "../utils/toktok-logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="Toktok logo"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
