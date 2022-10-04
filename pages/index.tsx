eba7e65cb2ceea7f1734361694d07bb67dc7f94b
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-10-04 11:24:21 +0200

    prepare backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import axios from "axios";
 import type { NextPage } from "next";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
