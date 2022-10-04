cf7a79646550ff14df2fab9ae83caf87f325f1f9
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-10-04 11:26:42 +0200

    connect to GoogleOauth API

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,13 @@
+import { GoogleLogin } from "@react-oauth/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { createOrGetUser } from "../utils";
 import Logo from "../utils/toktok-logo.png";
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -16,6 +20,23 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {user ? (
+          <div>Logged In </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => {
+              createOrGetUser(response);
+            }}
+            onError={() => {
+              console.log("error");
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 };
