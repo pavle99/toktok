ec36a0a56e5a19e8bf23d6ec9562c834540a3cac
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-10-15 23:27:29 +0200

    add Comments component

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { MdFavorite } from "react-icons/md";
 
@@ -7,14 +7,24 @@ import useAuthStore from "../store/authStore";
 interface IProps {
   handleLike: () => void;
   handleDislike: () => void;
+  likes: any[];
 }
 
-const LikeButton = ({ handleLike, handleDislike }: IProps) => {
-  const [alreadyLiked, setAlreadyLiked] = useState(true);
-  const { userProfile } = useAuthStore();
+const LikeButton = ({ handleLike, handleDislike, likes }: IProps) => {
+  const [alreadyLiked, setAlreadyLiked] = useState(false);
+  const { userProfile }: any = useAuthStore();
+
+  const filterLikes = likes?.filter((item) => item._ref === userProfile?._id);
+  useEffect(() => {
+    if (filterLikes?.length > 0) {
+      setAlreadyLiked(true);
+    } else {
+      setAlreadyLiked(false);
+    }
+  }, [filterLikes, likes]);
 
   return (
-    <div className="gap-6">
+    <div className="flex gap-6">
       <div className="mt-4 flex flex-col justify-center items-center cursor-pointer">
         {alreadyLiked ? (
           <div
@@ -31,7 +41,7 @@ const LikeButton = ({ handleLike, handleDislike }: IProps) => {
             <MdFavorite className="text-lg md:text-2xl" />
           </div>
         )}
-        <p className="text-md font-semibold">likes?.length || 0</p>
+        <p className="text-md font-semibold">{likes?.length || 0}</p>
       </div>
     </div>
   );
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -48,11 +48,13 @@ const Detail = ({ postDetails }: IProps) => {
 
   const handleLike = async (like: boolean) => {
     if (userProfile) {
-      const response = await axios.put(`${BASE_URL}/api/like`, {
+      const { data } = await axios.put(`${BASE_URL}/api/like`, {
         userId: userProfile._id,
         postId: post._id,
         like,
       });
+
+      setPost({ ...post, likes: data.likes });
     }
   };
 
@@ -135,8 +137,9 @@ const Detail = ({ postDetails }: IProps) => {
           <div className="mt-10 px-10">
             {userProfile && (
               <LikeButton
+                likes={post.likes}
                 handleLike={() => handleLike(true)}
-                handleDisike={() => handleLike(false)}
+                handleDislike={() => handleLike(false)}
               />
             )}
           </div>
